2eb5b91a7365cd9d2d9dc6e250d756cc2a01abb6
Marco Marziale <marco.marziale@outlook.fr>
2021-03-30 16:05:20 +0200

    visual update

--- src/DataFixtures/PropertyFixture.php
+++ src/DataFixtures/PropertyFixture.php
@@ -21,7 +21,7 @@ class PropertyFixture extends Fixture
                 ->setRooms($faker->numberBetween(2, 10))
                 ->setBedrooms($faker->numberBetween(1, 9))
                 ->setFloor($faker->numberBetween(0, 15))
-                ->setPrice($faker->numberBetween(100000, 1000000))
+                ->setPrice($faker->numberBetween(700, 3000))
                 ->setHeat($faker->numberBetween(0, count(Property::HEAT) - 1 ))
                 ->setCity($faker->city)
                 ->setAddress($faker->address)
